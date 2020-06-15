a6cbcdeebf3c924600288bac8c950dd70cb40e60
cofitost <zxcv091922@gmail.com>
2020-06-15 23:04:15 +0800

    modify preprocess

--- src/test/preprocess.js
+++ src/test/preprocess.js
@@ -4,11 +4,17 @@ const path = require('path');
 const testCase = "testSuite.json"
 const testFile = 'index.html';
 const data = JSON.parse(fs.readFileSync(testCase));
-const seleniumServer = process.env.WEB_SELENIUM_URL;
 const nginxServer = process.env.WEB_NGINX_URL;
-const mypath = nginxServer + '/src/web/html/' + testFile;
+const mypath = nginxServer + '/' + path.basename(path.resolve('../../')) + '/src/web/html/' + testFile;
 
 console.log(mypath);
 data.suites[0].cases[0].records[0].target.options[0].value = mypath;
 
 fs.writeFileSync(testCase, JSON.stringify(data, null, 4));
+
+const seleniumServer = process.env.WEB_SELENIUM_URL;
+const settings = "settings.json"
+const settingsData = JSON.parse(fs.readFileSync(settings));
+settingsData.webdriver.configs[0].serverUrl = seleniumServer;
+
+fs.writeFileSync(settings, JSON.stringify(settingsData, null, 4));
