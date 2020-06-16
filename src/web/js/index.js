d06dc0d33101dc2b8fccf2cf8672443b1eff6c23
cofitost <zxcv091922@gmail.com>
2020-06-16 16:03:38 +0800

    add error message

--- src/web/js/index.js
+++ src/web/js/index.js
@@ -2,12 +2,19 @@ function trans() {
   const input = document.getElementById("temperatureInputNum").value;
   const output = document.getElementById("temperatureOutputNum");
   const unit = document.getElementById("outputUnit");
+  const errorMsg = document.getElementById("errorMsg");
 
-  if (document.getElementById("toCelsius").checked === true) {
+  if (document.getElementById("toCelsius").checked === true && input != '') {
     output.innerHTML = (input - 32) / 9 * 5;
     unit.innerHTML = '℃';
-  } else if(document.getElementById("toFahrenheit").checked === true) {
+    errorMsg.innerHTML = '';
+  } else if(document.getElementById("toFahrenheit").checked === true && input != '') {
     output.innerHTML = (input / 5 * 9) + 32;
     unit.innerHTML = '℉';
+    errorMsg.innerHTML = '';
+  } else {
+    output.innerHTML = '';
+    unit.innerHTML = '';
+    errorMsg.innerHTML = 'Error!'
   }
 }
