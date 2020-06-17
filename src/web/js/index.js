8a43474a70b4704257d340a9a5e68a1366394558
cofitost <zxcv091922@gmail.com>
2020-06-17 23:20:46 +0800

    test

--- src/web/js/index.js
+++ src/web/js/index.js
@@ -1,16 +1,17 @@
 function trans() {
-  const input = document.getElementById("temperatureInputNum").value;
+  const inputC = document.getElementById("temperatureInputC").value;
+  const inputF = document.getElementById("temperatureInputF").value;
   const output = document.getElementById("temperatureOutputNum");
   const unit = document.getElementById("outputUnit");
   const errorMsg = document.getElementById("errorMsg");
 
-  if (document.getElementById("toCelsius").checked === true && input != '') {
-    output.innerHTML = (input - 32) / 9 * 5;
-    unit.innerHTML = '℃';
-    errorMsg.innerHTML = '';
-  } else if(document.getElementById("toFahrenheit").checked === true && input != '') {
-    output.innerHTML = (input / 5 * 9) + 32;
+  if (inputC != '' && inputF === '') {
+    output.innerHTML = (inputC / 5 * 9) + 32;
     unit.innerHTML = '℉';
+    errorMsg.innerHTML = '';
+  } else if(inputF != '' && inputC === '') {
+    output.innerHTML = (inputF - 32) * 5 / 9;
+    unit.innerHTML = '℃';
     errorMsg.innerHTML = '';
   } else {
     output.innerHTML = '';
