2a406700367821527910771f9694280c36c62cb1
cofitost <zxcv091922@gmail.com>
2020-06-18 17:41:04 +0800

    update

--- src/web/js/index.js
+++ src/web/js/index.js
@@ -1,19 +1,21 @@
 function trans() {
   const input = document.getElementById("temperatureInputNum").value;
   const output = document.getElementById("temperatureOutputNum");
+  const dot = document.getElementById("dot");
   const unit = document.getElementById("outputUnit");
   const errorMsg = document.getElementById("errorMsg");
 
   if (document.getElementById("toCelsius").checked === true && input != '') {
     output.innerHTML = (input - 32) / 9 * 5;
-    unit.innerHTML = '℃';
+    unit.innerHTML = 'C';
     errorMsg.innerHTML = '';
   } else if(document.getElementById("toFahrenheit").checked === true && input != '') {
     output.innerHTML = (input / 5 * 9) + 32;
-    unit.innerHTML = '℉';
+    unit.innerHTML = 'F';
     errorMsg.innerHTML = '';
   } else {
     output.innerHTML = '';
+    dot.innerHTML = '';
     unit.innerHTML = '';
     errorMsg.innerHTML = 'Error!'
   }
